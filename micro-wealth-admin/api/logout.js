9076eeb2688389e7c8ad9b1e945a34d863a52396
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-21 16:11:19 +1000

    logout.js

---
+++ micro-wealth-admin/api/logout.js
@@ -0,0 +1,6 @@
+import { clearSessionCookie, setJson } from './_utils/http.js'
+
+export default async function handler(_req, res) {
+  clearSessionCookie(res)
+  return setJson(res, 200, { ok: true })
+}

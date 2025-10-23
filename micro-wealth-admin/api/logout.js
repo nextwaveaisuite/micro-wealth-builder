1a4c70e8627788fda37abceaee2617eff4778c65
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-24 02:49:30 +1000

    Delete micro-wealth-admin directory

--- micro-wealth-admin/api/logout.js
+++
@@ -1,6 +0,0 @@
-import { clearSessionCookie, setJson } from './_utils/http.js'
-
-export default async function handler(_req, res) {
-  clearSessionCookie(res)
-  return setJson(res, 200, { ok: true })
-}
